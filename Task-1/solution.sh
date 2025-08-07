37e8adadc3e06593a0a765ee2658c912d3d488f4
rawan967 <rawan.tarek.atia58@gmaill.com>
2025-08-07 12:56:40 +0300

    Task submission.

---
+++ Task-1/solution.sh
@@ -0,0 +1,16 @@
+pwd
+ls
+cd gate-1
+rm trap
+cp key gate-2
+cd gate-2
+mv closed open
+mv open gate-3
+cd gate-3
+rm trap
+cd gate-4
+rm -r dimension-y dimension-x
+cd earth
+mkdir home
+touch home/bed
+
