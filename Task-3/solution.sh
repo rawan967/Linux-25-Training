0f96cbc1aef9b4bf324594dbf1ff4a5ac4cdf55f
rawan967 <rawan.tarek.atia58@gmail.com>
2025-08-13 03:53:39 +0300

    Task submission.

--- Task-3/solution.sh
+++ Task-3/solution.sh
@@ -1,1 +1,16 @@
+sudo useradd -m hero
+sudo useradd -m intruder
+sudo userdel -r intruder
+sudo groupadd warriors
+sudo groupadd healers
+sudo usermod -aG warriors hero
+sudo useradd -m sage
+sudo usermod -aG healers sage
+echo "CyberWorld's Core" > treasure.txt
+chmod 000 treasure.txt
+sudo chown hero treasure.txt
+sudo chmod 600 treasure.txt
+sleep 100 &
+ps -C sleep
+kill -19 3361
 
