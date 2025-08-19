e6856708c0d4928bb635de2e558da0c0e2f75084
rawan967 <rawan.tarek.atia58@gmail.com>
2025-08-19 21:50:39 +0300

    Task submission.

---
+++ Task-4/get_sum.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+is_positive(){
+if [[ $1 -gt 0 ]]; then 
+return 0
+else 
+return 1
+fi
+}
+
+sum=0
+
+
+while true; do
+read -p "Enter a number (0 to stop): " num 
+if [[ $num -eq 0 ]]; then
+break
+fi
+if is_positive "$num"; then
+sum=$((sum+num))
+fi
+done 
+echo "Sum of positive numbers: $sum" 
+
